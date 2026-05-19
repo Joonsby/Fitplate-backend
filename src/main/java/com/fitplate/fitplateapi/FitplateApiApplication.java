f0eeb85e6f8c7ae4abb455820f500817252b49e9
azsoft <flamewizkey@naver.com>
2026-05-19 12:33:46 +0900

    init: fitplate backend project setup

---
+++ src/main/java/com/fitplate/fitplateapi/FitplateApiApplication.java
@@ -0,0 +1,13 @@
+package com.fitplate.fitplateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitplateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitplateApiApplication.class, args);
+    }
+
+}
